62f4afb7b2ca6f80f5da0bd1250acc520d98fccd
geosen <43538334+geosen@users.noreply.github.com>
2022-06-03 14:49:33 +0300

    Add files via upload

--- R/ensg_to_hgnc.R
+++ R/ensg_to_hgnc.R
@@ -16,29 +16,32 @@
 #'@return A dataframe with deduplicated HGNC IDs as rownames
 #'@export
 
-ensg_to_hgnc <- function(table, ensembl_version = NULL, organism = 'hsapiens') {
+
+ensg_to_hgnc <- function(table, ensembl_version = 0, organism = 'hsapiens') {
+  
+  
   
   if(organism == 'hsapiens')
   {
     dataset = 'hsapiens_gene_ensembl'
-  
-  if (!is.null(ensembl_version)){
-    mart = useEnsembl(biomart = 'ensembl', dataset = dataset, version = ensembl_version)  
-    conv_table <- getBM(attributes = c('hgnc_symbol','ensembl_gene_id_version'), filters = 'ensembl_gene_id_version', values = rownames(table), mart = mart)
-    conv_table <- conv_table[!duplicated(conv_table$ensembl_gene_id_version),]
-    table$genes <- conv_table$hgnc_symbol[match(rownames(table),conv_table$ensembl_gene_id_version)]
-  
-  } else {
-    rownames(table) <- gsub('\\.[[:digit:]]{1+}$','',rownames(table))
-    mart = useEnsembl(biomart = 'ensembl', dataset = dataset)
-    conv_table <- getBM(attributes = c('hgnc_symbol','ensembl_gene_id'), filters = 'ensembl_gene_id', values = rownames(table), mart = mart)
-    conv_table <- conv_table[!duplicated(conv_table$ensembl_gene_id),]
-    table$genes <- conv_table$hgnc_symbol[match(rownames(table),conv_table$ensembl_gene_id)]
-  }
+    
+    if (ensembl_version != 0){
+      mart = useEnsembl(biomart = 'ensembl', dataset = dataset, version = ensembl_version)  
+      conv_table <- getBM(attributes = c('hgnc_symbol','ensembl_gene_id_version'), filters = 'ensembl_gene_id_version', values = rownames(table), mart = mart)
+      conv_table <- conv_table[!duplicated(conv_table$ensembl_gene_id_version),]
+      table$genes <- conv_table$hgnc_symbol[match(rownames(table),conv_table$ensembl_gene_id_version)]
+      
+    } else {
+      rownames(table) <- gsub('\\.[[:digit:]]{1+}$','',rownames(table))
+      mart = useEnsembl(biomart = 'ensembl', dataset = dataset)
+      conv_table <- getBM(attributes = c('hgnc_symbol','ensembl_gene_id'), filters = 'ensembl_gene_id', values = rownames(table), mart = mart)
+      conv_table <- conv_table[!duplicated(conv_table$ensembl_gene_id),]
+      table$genes <- conv_table$hgnc_symbol[match(rownames(table),conv_table$ensembl_gene_id)]
+    }
   } else if (organism == 'mmusculus') {
     
     dataset = 'mmusculus_gene_ensembl'
-    if (!is.null(ensembl_version)){
+    if (ensembl_version != 0){
       mart = useEnsembl(biomart = 'ensembl', dataset = dataset, version = ensembl_version)  
       conv_table <- getBM(attributes = c('mgi_symbol','ensembl_gene_id_version'), filters = 'ensembl_gene_id_version', values = rownames(table), mart = mart)
       conv_table <- conv_table[!duplicated(conv_table$ensembl_gene_id_version),]
@@ -53,8 +56,8 @@ ensg_to_hgnc <- function(table, ensembl_version = NULL, organism = 'hsapiens') {
     
     
   } else {
-      errorCondition(message = 'Invalid organism, please select hsapiens or mmusculus')
-    }
+    errorCondition(message = 'Invalid organism, please select hsapiens or mmusculus')
+  }
   
   #Discarding blank and NA hgnc symbols
   table = table[table$genes != '',]
@@ -63,32 +66,42 @@ ensg_to_hgnc <- function(table, ensembl_version = NULL, organism = 'hsapiens') {
   double_genes <- table$genes[which(duplicated(table$genes))]  
   table_no_genes <- table[,-length(table)]
   genes_to_remove  <- c()
-  for (i in 1:length(double_genes)) {
-    
-    dg <- double_genes[i]
-    possible_indices <- which(table$genes == dg)
-    
-    #subset the data frame with only the duplicate genes
-    dedup <- table[table$genes == dg,colnames(table)!='genes']
-    tdedup <- as.data.frame(t(dedup))
-    
-    #calculate mean expression and variance of duplicate genes
-    dmeans <- data.frame(colMeans(tdedup, na.rm=TRUE))
-    dvars <- data.frame(lapply(tdedup, var, na.rm = TRUE))
+  if(length(double_genes)>0){
     
     
-    #check which genes to keep
-    if(sum(dmeans > 1) == 1) {
-      #if only 1 gene has a mean expression over 1, remove all others 
-      genes_to_remove <-  c(genes_to_remove,possible_indices[dmeans <1])
-    } else {
-      #if 2 or more genes have expression over 1 or 2 or all genes have expression lower than 1 then keep the one with the highest variance
-      genes_to_remove <- c(genes_to_remove, possible_indices[dvars != max(dvars)])
+    for (i in 1:length(double_genes)) {
+      
+      dg <- double_genes[i]
+      possible_indices <- which(table$genes == dg)
+      
+      #subset the data frame with only the duplicate genes
+      dedup <- table[table$genes == dg,colnames(table)!='genes']
+      tdedup <- as.data.frame(t(dedup))
+      
+      #calculate mean expression and variance of duplicate genes
+      dmeans <- data.frame(colMeans(tdedup, na.rm=TRUE))
+      dvars <- data.frame(lapply(tdedup, var, na.rm = TRUE))
+      
+      
+      #check which genes to keep
+      if(sum(dmeans > 1) == 1) {
+        #if only 1 gene has a mean expression over 1, remove all others 
+        genes_to_remove <-  c(genes_to_remove,possible_indices[dmeans <1])
+      } else {
+        #if 2 or more genes have expression over 1 or 2 or all genes have expression lower than 1 then keep the one with the highest variance
+        genes_to_remove <- c(genes_to_remove, possible_indices[dvars != max(dvars)])
+      }
     }
-  }
-   
-   #deduplicate and return final table
+    
+    #deduplicate and return final table
     final_table <- table_no_genes[-genes_to_remove,]
     rownames(final_table) <- conv_table$hgnc_symbol[match(rownames(final_table),conv_table$ensembl_gene_id)]
-    return(final_table)
+    return(final_table) } else {
+      final_table <- table_no_genes
+      rownames(final_table) <- conv_table$hgnc_symbol[match(rownames(final_table),conv_table$ensembl_gene_id)]
+      return(final_table)
+      
+    }
+  
+  
 }
